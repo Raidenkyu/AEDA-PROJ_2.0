b9d86fb50656bd762ac4a22653913b39ee30d299
Unknown <joao-carlos.14@hotmail.com>
2017-10-16 19:30:54 +0100

    First commit

---
+++ headers/cruise.h
@@ -0,0 +1,15 @@
+/*
+ * cruise.h
+ *
+ *  Created on: 16/10/2017
+ *      Author: JCM, CV, JA
+ */
+
+#ifndef CRUISE_H_
+#define CRUISE_H_
+
+
+
+
+
+#endif /* CRUISE_H_ */
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 16/10/2017
+ *      Author: JCM, CV, JA
+ */
+
+
+
+
