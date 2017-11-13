d256260cca47313db9a0ac494601eb44bb360c9b
João Álvaro Ferreira <joaoalvaro.ferreira@hotmail.com>
2017-11-13 14:38:19 +0000

    Mais menus

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,14 @@
 
 #include <iostream>
 #include "cruise.h"
+#include "menu.h"
 
 using namespace std;
 
 
-
-void titulo() {
-
-
-	cout << "__________              __           __________.__                               _____                                             \n";
-	cout << "\\______   \\____________/  |_  ____   \\______   \\__|__  __ ___________  ______   /     \\ _____    ____ _____     ____   ___________ \n";
-	cout << " |     ___/  _ \\_  __ \\   __\\/  _ \\   |       _/  \\  \\/ // __ \\_  __ \\/  ___/  /  \\ /  \\\\__  \\  /    \\\\__  \\   / ___\\_/ __ \\_  __ \\ \n";
-	cout << " |    |  (  <_> )  | \\/|  | (  <_> )  |    |   \\  |\\   /\\  ___/|  | \\/\\___ \\  /    Y    \\/ __ \\|   |  \\/ __ \\_/ /_/  >  ___/|  | \\/\n";
-	cout << " |____|   \\____/|__|   |__|  \\____/   |____|_  /__| \\_/  \\___  >__|  /____  > \\____|__  (____  /___|  (____  /\\___  / \\___  >__|   \n";
-	cout << "                                             \\/              \\/           \\/          \\/     \\/     \\/     \\//_____/      \\/       \n";
-	cout << "\n";
-
-}
-
-void menuInicial()
-{
-	titulo();
-	cout << "\n";
-	cout << "\n";
-	cout << "Seja bem vindo ao gestor da Porto River, aqui poderá controlar todas as vertentes da sua empresa e visualizar toda a informação de que necessita. \n";
-
-}
-
 int main(){
 
 	menuInicial();
-		cin.get();
 	Empresa Porto_Rivers();
 	return 0;
 }
