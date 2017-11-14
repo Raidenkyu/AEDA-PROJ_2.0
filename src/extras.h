6e4255c353d0d6da55b57255dd4cd9b8b6e1d85b
Unknown <carlosmiguelvieira@hotmail.com>
2017-11-14 23:33:06 +0000

    Binary search and quick sort

--- src/extras.h
+++ src/extras.h
@@ -1,5 +1,6 @@
 #ifndef EXTRAS_H_
 #define EXTRAS_H_
+#include <vector>
 
 
                       /////                                          /////
@@ -23,7 +24,49 @@ class Schedule: public Time{
 
 };
 
+template<class T>
+void quickSort(std::vector<T> arr, int left, int right) {
+      int i = left, j = right;
+      int tmp;
+      int pivot = arr[(left + right) / 2];
 
+      /* partition */
+      while (i <= j) {
+            while (arr[i] < pivot)
+                  i++;
+            while (arr[j] > pivot)
+                  j--;
+            if (i <= j) {
+                  tmp = arr[i];
+                  arr[i] = arr[j];
+                  arr[j] = tmp;
+                  i++;
+                  j--;
+            }
+      };
+
+      /* recursion */
+      if (left < j)
+            quickSort(arr, left, j);
+      if (i < right)
+            quickSort(arr, i, right);
+}
+
+template <class T>
+int BinarySearch(const std::vector<T> &v, T x){
+	int left = 0, right = v.size() - 1;
+	while (left <= right){
+		int middle = (left + right) / 2;
+		if (v[middle] < x){
+			left = middle + 1;
+		}else if (x < v[middle]){
+			right = middle - 1;
+		}else{
+			return middle; // encontrou
+		}
+	return -1; // não encontrou
+	}
+}
 
 
 
